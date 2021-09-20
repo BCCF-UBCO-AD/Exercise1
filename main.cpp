2c59df6c346f13700538d145abe8f7a09e93ed18
iwanlevin <apple.iwan@gmail.com>
2021-09-20 09:46:02 -0700

    added the framework for parsing commands

--- main.cpp
+++ main.cpp
@@ -1,6 +1,31 @@
 #include <iostream>
+#include <getopt.h>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
+int main(int argc, char* argv[]) {
+
+    int opt;
+
+    while ((opt = getopt(argc, argv, "hedf")) != -1){
+
+        switch (opt)
+        {
+            case 'h':
+                printf("help command\n");
+                break;
+            case 'e':
+                printf("encrypt\n");
+                break;
+            case 'd':
+                printf("decrypt\n");
+                break;
+            case 'f':
+                printf("file path param\n");
+                break;
+            case 1:
+                printf("no arguments\n");
+                break;
+        }
+    }
+
     return 0;
 }
