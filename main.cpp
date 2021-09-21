f8c07aab0f20224990635d5df3b45d5a17a255c3
iwanlevin <apple.iwan@gmail.com>
2021-09-21 10:55:22 -0700

    update

--- main.cpp
+++ main.cpp
@@ -5,24 +5,24 @@ int main(int argc, char* argv[]) {
 
     int opt;
 
-    while ((opt = getopt(argc, argv, "hedf")) != -1){
+    while ((opt = getopt(argc, argv, "hedf")) != -1){ //getopt creates a data-structure that can be looped through from the argv array to process the parameters and their arguments
 
         switch (opt)
         {
             case 'h':
-                printf("help command\n");
+                printf("help command\n");    //placeholder
                 break;
             case 'e':
-                printf("encrypt\n");
+                printf("encrypt\n");         //placeholder
                 break;
             case 'd':
-                printf("decrypt\n");
+                printf("decrypt\n");         //placeholder
                 break;
             case 'f':
-                printf("file path param\n");
+                printf("file path param\n"); //file path
                 break;
             case 1:
-                printf("no arguments\n");
+                printf("no arguments\n");    //default non-argument case
                 break;
         }
     }
