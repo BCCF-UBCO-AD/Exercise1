252becd99db805e43094adf59b23eeb130ddedf6
Josh Cooper <cooper.s.josh@outlook.com>
2021-09-21 16:38:51 -0700

    Updates duplicate-detection test to use asserts, and a print line after

--- tests/duplicate-detection.cpp
+++ tests/duplicate-detection.cpp
@@ -1,18 +1,11 @@
 #include <iostream>
+#include <cassert>
 #include "common.h"
 #include "md5-duplicate-detection.h"
 
 int main(){
     fs::path A("test-files/A"), B("test-files/B"), E("test-files/E");
-    if(is_data_duplicate(A,B)){
-        std::cout << "Files A == B\n";
-    } else {
-        std::cout << "Files A != B\n";
-    }
-
-    if(is_data_duplicate(A,E)){
-        std::cout << "Files A == E\n";
-    } else {
-        std::cout << "Files A != E\n";
-    }
+    assert(is_data_duplicate(A,B));
+    assert(!is_data_duplicate(A,E));
+    std::cout << "test passed\n";
 }
