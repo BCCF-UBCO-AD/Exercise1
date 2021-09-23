e069834b2b7ed631b9f48d4a52c1b481db86e402
Raouf Tiliouine <hello@raouft.ca>
2021-09-23 12:48:03 +0300

    Add extern keyword to encrypt/decrypt headers

--- include/encrypt-decrypt.h
+++ include/encrypt-decrypt.h
@@ -2,6 +2,6 @@
 #include "common.h"
 #include <iostream>
 
-void encrypt(fs::path &input, fs::path &output);
+extern void encrypt(fs::path &input, fs::path &output);
 
-void decrypt(fs::path &input, fs::path &output);
+extern void decrypt(fs::path &input, fs::path &output);
