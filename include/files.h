9ce844b013fb63ec1275eabc09f9601ef011f34c
Iwan Levin <apple.iwan@gmail.com>
2021-09-23 11:30:50 -0700

    added short hand cmd parsing but ran into an segmentation fault error

--- include/files.h
+++ include/files.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <vector>
 #include "common.h"
 
 extern void getFiles(const char* directory, std::vector<fs::path> &files);
--- main.cpp
+++ main.cpp
@@ -1,28 +1,56 @@
 #include <iostream>
+#include <filesystem>
+#include <getopt.h>
+#include <vector>
 #include "common.h"
-#include "md5-duplicate-detection.h"
 #include "encrypt-decrypt.h"
 #include "files.h"
 
-int main(int argc, char* argv[]) {
+int main(int argc, char *argv[]) {
 
     int opt;
+    int eord = -1;
+    char directory;
+    std::vector<fs::path> files;
 
-    while ((opt = getopt(argc, argv, "hedf")) != -1){ //getopt creates a data-structure that can be looped through from the argv array to process the parameters and their arguments
+    while ((opt = getopt(argc, argv, "hedf")) !=-1) { //getopt creates a data-structure that can be looped through from the argv array to process the parameters and their arguments
 
-        switch (opt)
-        {
+        /*int option_index = 0;
+        static struct option long_options[] =
+                {
+                        {"help",     no_argument, NULL,  'h' },
+                        {"encrypt",  no_argument, NULL,  'e' },
+                        {"decrypt",  no_argument, NULL,  'd' },
+                        {"file",     required_argument,  NULL, 'f' },
+                        {NULL,      0,           NULL,  0  }
+                };
+        opt = getopt_long(argc, argv, "hedf", long_options, &option_index);
+
+        if(opt == -1)
+            break;
+*/
+        switch (opt) {
             case 'h':
                 printf("help command\n");    //placeholder
                 break;
             case 'e':
-                printf("encrypt\n");         //placeholder
+
+                if(eord == -1) {
+                    eord = 0;
+                }else{
+                    std::cout << "Choose either encrypt or decrypt but not both" << std::endl;
+                }
                 break;
             case 'd':
-                printf("decrypt\n");         //placeholder
+
+                if(eord == -1) {
+                    eord = 1;
+                }else{
+                    std::cout << "Choose either encrypt or decrypt but not both" << std::endl;
+                }
                 break;
             case 'f':
-                printf("file path param\n"); //file path
+                directory = *optarg;
                 break;
             case 1:
                 printf("no arguments\n");    //default non-argument case
@@ -30,21 +58,18 @@ int main(int argc, char* argv[]) {
         }
     }
 
-    /*
-    std::string directory = "some directory";
-    std::vector<fs::path> files;
-    getFiles(directory, files);
-    for (fs::path i: files)
-     std::cout << i.string() << ' ';
-     */
-
-    //    fs::path A("test-files/A");
-    //    fs::path B("test-files/B");
-    //    is_data_duplicate(A,B);
-
-    //    fs::path input("test-files/file.txt");
-    //    encrypt(input);
-    //    decrypt(input);
-
+    if(eord == 0){
+        getFiles(&directory, files);
+        for (fs::path &i: files) {
+            encrypt(i);
+            std::cout << "Encrypted " << i.string() << "\n";
+        }
+    }else if(eord == 1){
+        getFiles(&directory, files);
+        for (fs::path &i: files) {
+            decrypt(i);
+            std::cout << "Encrypted " << i.string() << "\n";
+        }
+    }else
     return 0;
 }
