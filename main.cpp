ebac62aeb6235fd874529b65b14bd389da76ea30
Iwan Levin <apple.iwan@gmail.com>
2021-09-23 18:55:30 -0700

    Error for no arguments found added.

--- main.cpp
+++ main.cpp
@@ -17,11 +17,13 @@ int main(int argc, char *argv[]) {
                     {"decrypt",  no_argument, NULL,  'd' },
                     {NULL,      0,    NULL,   0  }
             };
-
-
+    opt = getopt_long(argc, argv, "he:d:f:", long_options, &option_index);
+    if (opt == -1){
+        std::cout << "No arguments found use -h to see usage information" << std::endl;
+    }
     while (opt != -1) { //getopt creates a data-structure that can be looped through from the argv array to process the parameters and their arguments
 
-        opt = getopt_long(argc, argv, "he:d:f:", long_options, &option_index);
+
 
         switch (opt) {
             case 'h':
