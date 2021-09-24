7f668b1f8236d2a933d663bb384ec3d28007acba
Iwan Levin <apple.iwan@gmail.com>
2021-09-23 18:04:44 -0700

    Implemented the changes from the pull request comments and added file duplication check as an option from command line.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <getopt.h>
 #include "common.h"
+#include "md5-duplicate-detection.h"
 #include "encrypt-decrypt.h"
 #include "files.h"
 
@@ -9,7 +10,7 @@ int main(int argc, char *argv[]) {
     int opt, option_index = 0, eord = -1;
     const char *directory;
     std::vector<fs::path> files;
-    static struct option long_options[] =
+    struct option long_options[] =
             {
                     {"help",     no_argument, NULL,  'h' },
                     {"encrypt",  no_argument, NULL,  'e' },
@@ -20,31 +21,40 @@ int main(int argc, char *argv[]) {
 
     while (opt != -1) { //getopt creates a data-structure that can be looped through from the argv array to process the parameters and their arguments
 
-        opt = getopt_long(argc, argv, "he:d:", long_options, &option_index);
+        opt = getopt_long(argc, argv, "he:d:f:", long_options, &option_index);
 
         switch (opt) {
             case 'h':
-                printf("Usage: ex1 [OPTION]...[ARG]..\n"
+                printf("Usage: ex1 [OPTION]...[\"DIRECTORY\"]..\n"
                        "Used to encrypt and decrypt directories given a root path as the argument. You can only\n"
                        "use on argument at a time encrypt or decrypt\n\n"
                        "options:\n"
-                       "-e, --encrypt     | Encrypts files in a given directory and accepts the root path as an argument \n"
-                       "-d, --decrypt     | Decrypts encrypted files from this program and accepts the root path as an argument\n"
-                       "-h, --help        | Prints help message\n"
+                       "-e, --encrypt               | Encrypts files in a given directory and accepts the root path as an argument \n"
+                       "-d, --decrypt               | Decrypts encrypted files from this program and accepts the root path as an argument\n"
+                       "-f                          | Checks for duplicate files in a directory\n"
+                       "-h, --help                  | Prints help message\n"
                 );    //placeholder
                 break;
             case 'e':
+                directory = optarg;
                 if (eord < 0) {
-                    directory = optarg;
                     eord = 0;
                 }else{
                     eord = 2;
                 }
                 break;
             case 'd':
-                if (eord < 0) {
                 directory = optarg;
-                eord = 1;
+                if (eord < 0) {
+                    eord = 1;
+                }else{
+                    eord = 2;
+                }
+                break;
+            case 'f':
+                directory = optarg;
+                if (eord < 0) {
+                    eord = 3;
                 }else{
                     eord = 2;
                 }
@@ -55,20 +65,32 @@ int main(int argc, char *argv[]) {
         }
     }
 
+    getFiles(directory, files);
+
     if(eord == 0){
-        getFiles(directory, files);
-        for (fs::path &i: files) {
-            encrypt(i);
-            std::cout << "Encrypted " << i.string() << "\n";
+        for (fs::path &file: files) {
+            encrypt(file);
+            std::cout << "Encrypted " << file.string() << "\n";
         }
     }else if(eord == 1){
-        getFiles(directory, files);
-        for (fs::path &i: files) {
-            decrypt(i);
-            std::cout << "Decrypted " << i.string() << "\n";
+        for (fs::path &file: files) {
+            decrypt(file);
+            std::cout << "Decrypted " << file.string() << "\n";
         }
     }else if(eord == 2){
-        std::cout << "Must choose either encrypt or decrypt" << std::endl;
+        std::cout << "Must choose either encrypt or decrypt or file duplication check" << std::endl;
+    }else if(eord ==3){
+
+        for(fs::path &file1: files){
+            for(fs::path &file2: files){
+                if(file1 == file2) //compares file objects to ensure it doesn't compare to itself.
+                    break;
+                if(is_data_duplicate(file1, file2)){
+                    std::cout << file1.string() << " and " << file2.string() << " are duplicates.\n";
+                }
+            }
+        }
+
     }
 
     return 0;
